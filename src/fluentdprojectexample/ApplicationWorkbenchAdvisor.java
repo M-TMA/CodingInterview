a056eabe29519a0892702304d3c67965b0736190
tnhminh075 <tnhminh075@gmail.com>
2016-06-28 14:14:58 +0700

    FluentProjectExample_Ver1

---
+++ src/fluentdprojectexample/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package fluentdprojectexample;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "FluentdProjectExample.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
