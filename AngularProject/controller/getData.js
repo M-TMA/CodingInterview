fa2160b7c4f7e676bd37a8a308ae7a3b162528f2
tnhminh <tnhminh075@gmail.com>
2016-06-29 16:00:21 +0700

    add employer successfully

--- AngularProject/controller/getData.js
+++ AngularProject/controller/getData.js
@@ -6,11 +6,38 @@ app.controller('getDataController', function($scope, $http) {
     .then(function(response) {
         $scope.fluentd = response.data;
     });
-}).controller('getAllDataController', function ($scope,$http){
-	debugger;
+})
+
+.controller('getAllDataController', function ($scope,$http){
+	//debugger;
     $http.get('http://192.168.95.222:9200/bank/account/_search?size=30')
     .then(function(response) {
         $scope.accounts = response.data.hits.hits;
 		console.log($scope.accounts);
     });
+	
+	
+})
+
+.controller('addNewController', function ($scope,$http){
+	//debugger;
+$scope.add = function (){
+	debugger;
+    var data = JSON.stringify({firstname:$scope.firstname,lastname:$scope.lastname,age:$scope.age,gender:$scope.gender,address:$scope.address, employer: $scope.employer, email:$scope.email}, null);
+		var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+	$http.post('http://192.168.95.222:9200/bank/account?pretty',data,config)
+   .then(
+       function(response){
+		   $scope.account = response.data;
+         // success callback
+       }, 
+       function(response){
+         // failure callback
+       }
+    );
+}
 });
