c906cae615cdc3304f595375847cb90eb75432b5
tnhminh <tnhminh075@gmail.com>
2016-06-30 19:42:16 +0700

    Changed on GUI & controller

--- AngularProject/controller/data_controller.js
+++ AngularProject/controller/data_controller.js
@@ -2,17 +2,32 @@ var app = angular.module('myApp', []);
 app.controller('ProcessDataController', function ($scope,$http){
 	//debugger;
 	
+	// GET LIST emp
 	$scope.initFirst = function(){
-    $http.get('http://192.168.95.222:9200/bank/account/_search?size=30')
+	 $http.get('http://192.168.95.222:9200/bank/account/_search?size=40')
     .then(function(response) {
         $scope.accounts = response.data.hits.hits;
-		     $scope.myVariable=response;
-
     });
 };
-	
+	//ADD new emp
 	$scope.add = function (){
-    var data = JSON.stringify({firstname:$scope.firstname,lastname:$scope.lastname,age:$scope.age,gender:$scope.gender,address:$scope.address, employer: $scope.employer, email:$scope.email}, null);
+		var newObject = {
+			 _source : {
+			"account_number":$scope.accountno,
+			"firstname":$scope.firstname,
+			"lastname":$scope.lastname,
+			"age":$scope.age,
+			"gender":$scope.gender,
+			"address":$scope.address,
+			"employer": $scope.employer,
+			"email":$scope.email
+			}
+		}
+		if (newObject!=null){
+			$scope.accounts.push(newObject);
+		}
+    var jsonData = JSON.stringify({firstname:$scope.firstname,lastname:$scope.lastname,age:$scope.age,gender:$scope.gender,address:$scope.address, employer: $scope.employer, email:$scope.email}, null);
+		// var jsonData = JSON.stringify(newObject);
 		var config = {
                 headers : {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
@@ -20,20 +35,39 @@ app.controller('ProcessDataController', function ($scope,$http){
             }
 			if (($scope.firstname && $scope.lastname && $scope.age && $scope.gender && $scope.address && $scope.employer && $scope.email) != null ) {
 			
-			
-	$http.post('http://192.168.95.222:9200/bank/account?pretty', data, config)
+	$http.post('http://192.168.95.222:9200/bank/account?pretty', jsonData, config)
    .then(
        function(response){
 		   // success callback
 		   $scope.account = response.data;
-		   $scope.myVariable=response;
-		   $scope.initFirst();
        }, 
        function(response){
          // failure callback
        }
     );
  }
+}
+
+// Delete Employer
+$scope.deleteEmp = function (account){
+	
+	var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+	var urlDel = "http://192.168.95.222:9200/bank/account/" + account._id + "?pretty";
+	$http.delete(urlDel, config)
+   .then(
+       function(response){
+		   // success callback
+		var index = $scope.accounts.indexOf(account);
+		$scope.accounts.splice(index, 1);
+       }, 
+       function(response){
+         // failure call back
+       }
+    );
 }
 	
 })
