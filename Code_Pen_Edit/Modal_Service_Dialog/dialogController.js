6598841d00c0c6fc9e500de382237959b0c9f7df
tnhminh <tnhminh075@gmail.com>
2016-07-15 17:44:48 +0700

    completed dialog modal

--- Code_Pen_Edit/Modal_Service_Dialog/dialogController.js
+++ Code_Pen_Edit/Modal_Service_Dialog/dialogController.js
@@ -2,14 +2,15 @@ var app = angular.module('app', ['angularModalService']);
 
 app.controller('Controller', function ($scope, ModalService) {
     "use trict";
+    //debugger;
     $scope.show = function () {
         ModalService.showModal({
             templateUrl: 'modal.html',
             controller: "ModalController"
         }).then(function (modal) {
-            modal.element.modal();
+            modal.element.modal(); // open modal dialog
             modal.close.then(function (result) {
-                $scope.message = "You said " + result;
+                $scope.message = result;
             });
         });
     };
@@ -17,9 +18,19 @@ app.controller('Controller', function ($scope, ModalService) {
 });
 
 app.controller('ModalController', function ($scope, close) {
-
-    $scope.close = function (result) {
-        close(result, 500); // close, but give 500ms for bootstrap to animate
+    debugger;
+    $scope.Yes = function () {
+        result = "Right, Your name is " + $scope.name;
+        close(result, 300); // close, but give 500ms for bootstrap to animate
     };
 
+    $scope.No = function () {
+        result = "You choose NO, please try again"
+        close(result, 300); // close, but give 500ms for bootstrap to animate
+    };
+
+    $scope.Cancel = function () {
+        result = "You can't type input";
+        close(result, 500); // close, but give 500ms for bootstrap to animate
+    };
 });
